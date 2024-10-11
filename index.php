905bd237275a77da25ca24b04553bbf104a1a8f8
Frost-0214 <jmgasangue@gmail.com>
2024-10-11 18:08:18 +0800

    [Day 1] Variables 2

--- index.php
+++ index.php
@@ -10,7 +10,7 @@
     // $name = "John Doe";
     // $Name = "Jane Doe";
 
-    // //code declaration
+    // // Concatenation
     // $firstName = "John";
     // $name = "$firstName Doe";
     
@@ -28,10 +28,170 @@
     // echo "<br>";
     //print $Name;
 
-    $cars = ["honda" , "toyota" , "ford"];
+    //Array Data Type
+    // $cars = ["honda" , "toyota" , "ford"];
 
-    var_dump($cars[1]);
-    echo $cars[0];
+    // var_dump($cars[1]);
+    // echo $cars[1];
+
+    // $age = 20.5;
+
+    // var_dump($age);
+
+    // $isPass = true;
+    // $isPass = null;
+    // var_dump($isPass);
+
+    // $firstName = "John";
+    // echo "$firstName Doe";
+    // echo "<br>";
+    // echo '$firstName Doe';
+
+    // $name = "John Doe";
+    // echo strpos($name);
+    
+    // var_dump(str_word_count("Hello World!"));
+    // var_dump(strpos("Hello world!", "world"));
+
+    // $x = "                Hello World!";
+    // echo trim($x);
+
+    // $x = "Hello World!";
+    // $y = explode(" ", $x);
+
+    // var_dump($y[0]);
+
+    // $x = "$x We are the so-called \"Vikings\" from the north.";
+    // echo $x;
+
+    // $x = "20.50";
+    // $y = 80.80;
+
+    // echo $x + $y;
+    
+    // var_dump(PHP_INT_MAX);
+
+    // $x = 1.9e411;
+    // var_dump(is_infinite($x));
+
+    // $x = "grade 20";
+    // $x = (int)$x;
+    // var_dump($x);
+    
+    // // Round Up Decimal Number
+    // $x = 20.6983;
+    // $round = round($x);
+    // echo $round;
+
+    // // Round Up Specific Decimal Number
+    // $x = 20.6983;
+    // $round = round($x, 3);
+    // echo $round;
+
+    // echo rand(10, 100);
+
+    // define("GREETING", "Welcome to PHP!");
+    // echo GREETING;
+
+    // const MYCAR = "Volvo";
+    // echo MYCAR;
+
+    // Grading System
+    /**
+     * 74 Below = Failed
+     * 75 Above = Passed
+     * 90 Above = Excellent
+     */
+    // $grade = 75;
+    // if ($grade <= 74) {
+    //     echo "Failed";
+    // }
+    // else if ($grade >= 90 ) {
+    //     echo "Excellent";
+    // }
+    // else {
+    //     echo "Passed";
+    // }
+
+    // One Line IfElse Statement
+    // $isPassed = (true) ? "Passed" : "Failed";
+    // echo $isPassed;
+
+    // Database If Data is Null
+    // $database = 30;
+    // $database = null;
+    // $age = $database ?? 20;
+    // echo $age;
+
+    // Switch Statement
+    // $status = "paid";
+    // switch ($status) {
+    //     case 'active' or 'paid':
+    //         echo "Welcome to Dashboard";
+    //         break;
+    //     case 'pending':
+    //         echo "Please verify your email";
+    //         break;
+    //     default:
+    //         echo "Invalid Account";
+    //         break;
+    // }
+
+    // While Loop and Display
+    // $x = 1;
+    // while($x < 6) {
+    //     echo "$x <br>";
+    //     $x++;
+    // }
+
+    // DoWhile Loop and Display
+    // $x = 1;    
+    // do {
+    //     echo "$x <br>";
+    //     $x++;
+    // } while ($x < 6);
+
+    // For Loop and Display
+    // for($x = 1; $x < 6; $x++) {
+    //     echo "$x <br>";
+    // }
+
+    // ForEach Loop and Display
+    // $colors = array("red", "green", "blue", "yellow");
+    // foreach ($colors as $x) {
+    //     echo "$x <br>";
+    // }
+
+    // Half Pyramid
+    // $x = 5;
+    // for($y = 1; $y <= $x; $y++) {
+        // for($z = 1; $z <= $y; $z++) {
+            // echo "* ";
+        // }
+        // echo "<br>";        
+    // }
+    
+    // Half Pyramid
+    // $count = 5;
+    // $result = "";
+    // for($x = 1; $x <= $result; $x++) {
+    //     for($y = 1; $y <= $x; $y++) {
+    //         $result .= "*";
+    //     }
+    //     $result .= "<br>" ;
+    // }
+    // echo $result;
+
+    $cars = [
+        [
+            "name" => null ?? "John"
+        ],
+        [
+            "name" => "Jane"
+        ]
+    ];
+    echo $cars[0]["name"];
+
    ?>
 </body>
 </html>
