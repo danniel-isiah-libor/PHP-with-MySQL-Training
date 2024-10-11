dc0c0ce1a4544605ee259c4622cc4dc717ab42aa
Kyle Carlos <kyleandreic@gmail.com>
2024-10-11 18:08:22 +0800

    update

--- index.php
+++ index.php
@@ -10,7 +10,6 @@
 <h1>
     <?php
 
-
     $age = 25;
     $lmao = 30 + $age;
 
@@ -19,12 +18,48 @@
 
     $testArray = [$age, $lmao, $name];
 
+const failed = 70;
+const passed = 75;
+const excellent = 90;
+
+$grade = 90;
+
+if ($grade<passed) {
+    echo "Failed";
+} elseif ($grade>= passed and $grade<excellent) {
+    echo "Passed";
+} elseif ($grade>=excellent) {
+    echo "Excellent";
+}
+
+echo "<br>";
+echo "<br>";
+echo "<br>";
+
+$pyr = 4;
+const slash  = "*";
+
+for ( $i = 0; $i < $pyr; $i++){
+    for($y = 1; $y <= $i; $y++){
+echo slash;
+    }
+    echo slash . "<br>";
+}
+
+echo "<br>";
+
+
     echo $lmao;
     echo $name;
     echo "<br>";
     echo $Name;
-    var_dump($testArray)
+    var_dump($testArray);
+    echo "<br>";
+    echo "<br>";
+
+
     ?>
+
 </h1>
 
 </body>
