b228673a46ef06b3de3bc875efcc9ff66e4af3df
Danniel Libor <dlibor.dev@gmail.com>
2024-10-11 13:53:21 +0800

    [Day 1] variables

--- index.php
+++ index.php
@@ -8,9 +8,38 @@
 </head>
 
 <body>
-    <h1>
-        Hello World
-    </h1>
+    <?php
+    // $name = "John Doe"; 
+    /**
+     * a
+     * b
+     * c
+     */
+    # $Name = "Jane Doe";
+
+    // // Concatenation
+    // $firstName = "John";
+    // $name = "$firstName Doe";
+
+    // // Reassigning
+    // $name = "John Doe";
+    // $name = "Jane Doe";
+
+    // $age = $grade = 75;
+
+    // $grade = 20;
+
+    // echo $age;
+
+    // echo $name;
+    // echo "<br>";
+    // print $Name;
+
+    $cars = ["honda", "toyota", "ford"];
+
+    var_dump($cars[1]);
+    echo $cars[1];
+    ?>
 </body>
 
 </html>
