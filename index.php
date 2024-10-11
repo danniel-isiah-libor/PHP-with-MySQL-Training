ae801aa04cc2f47e965aaf70d0abcb5237230815
Kyle Carlos <kyleandreic@gmail.com>
2024-10-11 15:49:40 +0800

    test push

--- index.php
+++ index.php
@@ -1,16 +1,31 @@
 <!DOCTYPE html>
 <html lang="en">
-
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
-
 <body>
-    <h1>
-        Hello World!
-    </h1>
-</body>
+    
+<h1>
+    <?php
 
+
+    $age = 25;
+    $lmao = 30 + $age;
+
+    $name = "Kyle";
+    $Name = "Jane Doe";
+
+    $testArray = [$age, $lmao, $name];
+
+    echo $lmao;
+    echo $name;
+    echo "<br>";
+    echo $Name;
+    var_dump($testArray)
+    ?>
+</h1>
+
+</body>
 </html>
