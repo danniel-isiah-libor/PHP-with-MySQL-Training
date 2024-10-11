ea714bbf81564ea143063e123f6e2c7991b34019
Danniel Libor <dlibor.dev@gmail.com>
2024-10-11 15:28:50 +0800

    [Day 1] variables

--- index.php
+++ index.php
@@ -35,10 +35,53 @@
     // echo "<br>";
     // print $Name;
 
-    $cars = ["honda", "toyota", "ford"];
+    // Array data type
+    // $cars = ["honda", "toyota", "ford"];
+    // var_dump($cars[1]);
+    // echo $cars[1];
 
-    var_dump($cars[1]);
-    echo $cars[1];
+    // Float data type
+    // $age = 20.5;
+    // var_dump($age);
+
+    // $isPass = true;
+    // $isPass = null;
+    // var_dump($isPass);
+
+    // $firstName = "John";
+    // echo "$firstName Doe";
+    // echo "<br>";
+    // echo '$firstName Doe';
+
+    // $name = "John Doe";
+    // var_dump(strlen($name));
+
+    // var_dump(str_word_count("Hello World"));
+    // var_dump(strpos("Hello world!", "world"));
+
+    // $x = "Hello World!";
+    // echo strrev($x);
+
+    // $x = "                Hello World!                          ";
+    // echo trim($x);
+
+    $x = "Hello World!";
+    // $y = explode(" ", $x);
+
+    // var_dump($y[0]);
+
+    // $x = "$x We are the so-called \"Vikings\" from the north.";
+    // echo $x;
+
+    // $x = "20.50";
+    // $y = 80.80;
+
+    // echo $x + $y;
+
+    // var_dump(PHP_INT_MAX);
+
+    $x = 1.9e411;
+    var_dump(is_infinite($x));
     ?>
 </body>
 
