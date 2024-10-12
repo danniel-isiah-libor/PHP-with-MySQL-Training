933dc5412c008019b6baaffa2f7fa69d59d2b6ae
Danniel Libor <dlibor.dev@gmail.com>
2024-10-12 18:06:00 +0800

    [Day 2] OOP

--- OOP/ProcessRegister.php
+++ OOP/ProcessRegister.php
@@ -1,38 +1,156 @@
 <?php
 
-require_once "User.php";
-
-class ProcessRegister extends User
+class ProcessRegister
 {
+    public $name,
+        $email,
+        $password,
+        $passwordConfirmation,
+        $errors;
+
+    /**
+     * constructor
+     */
     public function __construct()
     {
-        echo "Initialization... <br>";
+        if (!isset($_SESSION)) session_start();
+
+        $this->name = $_POST['name'];
+        $this->email = $_POST['email'];
+        $this->password = $_POST['password'];
+        $this->passwordConfirmation = $_POST['password_confirmation'];
+
+        $this->errors = [];
     }
 
+    /**
+     * check if user already logged in
+     */
+    public function authorization()
+    {
+        if (isset($_SESSION['auth'])) {
+            header("Location: index.php");
+            die();
+        }
+
+        if ($_SERVER['REQUEST_METHOD'] === "GET") {
+            header("Location: register.php");
+            die();
+        }
+
+        return $this;
+    }
+
+    /**
+     * validate form fields
+     */
     public function validate()
     {
-        // var_dump($this->getUser());
+        $this->validateName();
+        $this->validateEmail();
+        $this->validatePassword();
 
-        // var_dump($this->getProfile());
+        if (count($this->errors) > 0) {
+            $_SESSION['errors'] = $this->errors;
 
-        // $this->save();
+            header("Location: register.php");
+            die();
+        }
 
-        echo self::DATABASE_PASSWORD;
+        $_SESSION['errors'] = [];
 
-        echo "Validating... <br>";
+        return $this;
+    }
+
+    /**
+     * required
+     * string
+     * max 50
+     */
+    private function validateName()
+    {
+        if (empty($this->name)) {
+            $this->errors['name'][] = "Name is required";
+        }
+
+        if ((int)$this->name && is_numeric((int)$this->name)) {
+            $this->errors['name'][] = "Name is invalid";
+        }
+
+        if (strlen($this->name) > 50) {
+            $this->errors['name'][] = "Name is too long";
+        }
+
+        return $this;
+    }
+
+    /**
+     * required
+     * email
+     * unique
+     */
+    private function validateEmail()
+    {
+        if (empty($this->email)) {
+            $this->errors['email'][] = "Email is required";
+        }
+
+        if (!filter_var($this->email, FILTER_VALIDATE_EMAIL)) {
+            $this->errors['email'][] = "Email is invalid format";
+        }
+
+        return $this;
+    }
+
+    /**
+     * required
+     * min 8
+     * max 12
+     */
+    private function validatePassword()
+    {
+        if ($this->password !== $this->passwordConfirmation) {
+            $this->errors['password'][] = "Password does not match";
+        }
+
+        if (
+            strlen($this->password) < 8 ||
+            strlen($this->password) > 12
+        ) {
+            $this->errors['password'][] = "Password must be between 8 and 12 characters";
+        }
+
+        return $this;
     }
 
     public function save()
     {
         // saving...
+
+        return $this;
     }
 
-    public function __destruct()
+    /**
+     * set session auth
+     */
+    public function authentication()
     {
-        echo "Close connection...";
+        $_SESSION['auth'] = [
+            'name' => $this->name,
+            'email' => $this->email
+        ];
+
+        return $this;
+    }
+
+    /**
+     * redirect to index.php
+     */
+    public function redirection()
+    {
+        header("Location: index.php");
+        die();
+
+        return $this;
     }
 }
-
-$class = new ProcessRegister();
-$class->validate();
-// echo $class::DATABASE_PASSWORD;
--- process-register.php
+++ process-register.php
@@ -1,121 +1,12 @@
 <?php
 
-if (!isset($_SESSION)) session_start();
+require_once "./OOP/ProcessRegister.php";
 
-if (isset($_SESSION['auth'])) {
-    header("Location: index.php");
-    die();
-}
+$processRegisterClass = new ProcessRegister();
 
-if ($_SERVER['REQUEST_METHOD'] === "GET") {
-    header("Location: register.php");
-    die();
-}
-
-// Initialization...
-$name = $_POST['name'];
-$email = $_POST['email'];
-$password = $_POST['password'];
-$passwordConfirmation = $_POST['password_confirmation'];
-
-$errors = [];
-
-// Validation...
-validate();
-
-if (count($errors) > 0) {
-    $_SESSION['errors'] = $errors;
-
-    header("Location: register.php");
-    die();
-}
-
-// saving ...
-
-$_SESSION['errors'] = [];
-
-// authentication ...
-$_SESSION['auth'] = [
-    'name' => $name,
-    'email' => $email
-];
-
-// redirection...
-header("Location: index.php");
-die();
-
-function validate()
-{
-    validateName();
-    validateEmail();
-    validatePassword();
-}
-
-/**
- * required
- * string
- * max 50
- */
-function validateName()
-{
-    global $name, $errors;
-
-    if (empty($name)) {
-        $errors['name'][] = "Name is required";
-    }
-
-    if ((int)$name && is_numeric((int)$name)) {
-        $errors['name'][] = "Name is invalid";
-    }
-
-    if (strlen($name) > 50) {
-        $errors['name'][] = "Name is too long";
-    }
-
-    return;
-}
-
-/**
- * required
- * email
- * unique
- */
-function validateEmail()
-{
-    global $email, $errors;
-
-    if (empty($email)) {
-        $errors['email'][] = "Email is required";
-    }
-
-    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-        $errors['email'][] = "Email is invalid format";
-    }
-
-    return;
-}
-
-/**
- * required
- * min 8
- * max 12
- */
-function validatePassword()
-{
-    global $password,
-        $passwordConfirmation,
-        $errors;
-
-    if ($password !== $passwordConfirmation) {
-        $errors['password'][] = "Password does not match";
-    }
-
-    if (
-        strlen($password) < 8 ||
-        strlen($password) > 12
-    ) {
-        $errors['password'][] = "Password must be between 8 and 12 characters";
-    }
-
-    return;
-}
+$processRegisterClass
+    ->authorization()
+    ->validate()
+    ->save()
+    ->authentication()
+    ->redirection();
