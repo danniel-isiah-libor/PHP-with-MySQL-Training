432e0f45245c32ed39f192a07e90865d8349d8b3
Danniel Libor <dlibor.dev@gmail.com>
2024-10-12 10:16:48 +0800

    [Day 2] functions

--- index.php
+++ index.php
@@ -80,8 +80,163 @@
 
     // var_dump(PHP_INT_MAX);
 
-    $x = 1.9e411;
-    var_dump(is_infinite($x));
+    // $x = 1.9e411;
+    // var_dump(is_infinite($x));
+
+    // $x = "John";
+    // $x = (array)$x;
+    // var_dump($x);
+
+    // $array = [
+    //     "name" => "John Doe",
+    //     "age" => 20,
+    //     "isPass" => true
+    // ];
+    // $array = (object)$array;
+    // var_dump($array);
+
+    // $x = 20.6983;
+    // $round = round($x, 3);
+
+    // echo rand(10, 100);
+
+    // const MYCAR = "Volvo";
+    // echo MYCAR;
+
+    // $x = 20;
+    // $y = 10;
+
+    // var_dump($x <=> $y);
+
+    /**
+     * 74 below = failed
+     * 75 above = passed
+     * 90 above = excellent
+     */
+    // $grade = 0;
+
+    // if ($grade >= 90) {
+    //     echo "Excellent";
+    // } else if ($grade >= 75) {
+    //     echo "Passed";
+    // } else {
+    //     echo "Failed";
+    // }
+
+    // $x = ["honda", "toyota", 'ford'];
+    // $y = ["chevy", "mitshubishi", "volvo", "test"];
+
+    // var_dump($x + $y);
+
+    // $isPassed = (true) ? ((true) ? "passed" : "failed") : "failed";
+
+    // $database = 0;
+    // $age = $database ?? 20;
+
+    // echo $age;
+
+    // $status = "paid";
+
+    // switch ($status) {
+    //     case 'active' or 'paid':
+    //         echo "Welcome to dashboard";
+    //         break;
+
+    //     case 'pending':
+    //         echo "Please verify your email";
+    //         break;
+
+    //     default:
+    //         echo "Invalid account";
+    //         break;
+    // }
+
+    // $x = 1;
+    // do {
+    //     echo "$x <br>";
+    //     $x++;
+    // } while ($x < 6)
+
+    // $count = 5;
+    // for ($x = 1; $x <= $count; $x++) {
+    //     $result = "";
+    //     for ($y = 1; $y <= $x; $y++) {
+    //         $result .= "*";
+    //         // $result = $result . "*";
+    //     }
+    //     $result .= "<br>";
+    //     echo $result;
+    // }
+    // for ($x = 1; $x <= $count; $x++) {
+    //     $result .= str_repeat('*', $x) . "<br>";
+    // }
+    // echo $result;
+
+    $profile = "Dan";
+
+    // $cars = [
+    //     [
+    //         "name" => $profile ?: "John"
+    //     ],
+    //     [
+    //         "name" => "Jane"
+    //     ]
+    // ];
+
+    // echo $cars[0]["name"];
+
+    // $array = [1, 2, 3];
+    // // $array[] = [4, 5];
+    // array_push($array, [4, 5]);
+    // var_dump($array);
+
+    // $cars = array("Volvo", "BMW", "Toyota");
+    // array_splice($cars, 0, 2);
+    // var_dump($cars);
+
+    // function calculateGrades($profile = [])
+    // {
+    //     $x = 1;
+    //     $y = 1;
+
+    //     echo $profile;
+
+    //     return $x + $y;
+    // }
+
+    // $student = [
+    //     'name' => 'John',
+    //     'age' => 20,
+    //     'address' => 'Philippines'
+    // ];
+    // $grades = calculateGrades($student);
+
+    // echo $grades; // sum
+
+    $globalScope = "Jane Doe";
+    $localScope = null;
+
+    $count = 1;
+
+    echo __LINE__;
+    echo "<br>";
+    require "not_found.php";
+    echo "<br>";
+    echo __LINE__;
+    echo "<br>";
+    include_once "profile.php";
+    echo "<br>";
+    echo __LINE__;
+    echo "<br>";
+
+    echo $globalScope . "<br>";
+    getProfile();
+    getProfile();
+    getProfile();
+    echo $globalScope . "<br>";
+    echo $count;
+
+    echo $localScope;
     ?>
 </body>
 
