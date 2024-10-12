7981a8db1d70bb4cd05800a3b42f0d83f8e6aaf1
Danniel Libor <dlibor.dev@gmail.com>
2024-10-12 11:42:29 +0800

    Day 2[ login]

---
+++ header.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-light bg-light">
+    <a class="navbar-brand" href="/playground/index.php">Home</a>
+
+    <a class="navbar-brand" href="/playground/profile.php">Profile</a>
+
+    <a class="navbar-brand" href="/playground/login.php">Logout</a>
+</nav>
--- profile.php
+++ profile.php
@@ -1,17 +1,16 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
 
-function getProfile()
-{
-    global $globalScope, $count, $localScope;
-    // global $count;
+<?php require_once "head-tag.php"; ?>
 
-    $globalScope = "John Doe";
+<body>
+    <?php require_once "header.php"; ?>
 
-    $localScope = "Hello World";
+    <h1>Profile Page</h1>
 
-    static $count = 1;
+    <?php require_once "footer.php"; ?>
 
-    $count++;
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+</body>
 
-    echo $count;
-}
+</html>
